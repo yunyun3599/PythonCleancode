b0491d727641b0987b73644a93f4172fc224eb8e
yoonjae <yunyun3599@gmail.com>
2023-04-27 07:33:07 +0900

    제너레이터 - 값을 return하는 경우

---
+++ chapter7/generator_return_value.py
@@ -0,0 +1,14 @@
+def generator():
+    yield 1
+    yield 2
+    return 3
+
+
+if __name__ == "__main__":
+    gen = generator()
+    print(next(gen))
+    print(next(gen))
+    try:
+        print(next(gen))
+    except  StopIteration as e:
+        print(f"returned value: {e.value}")     # returned value: 3
